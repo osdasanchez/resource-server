2e995590d6984c3420f00bccfeee1ef4b3310e6a
Oscar Daniel Sanchez Soto <osdasanchez@outlook.com>
2023-12-13 02:42:52 -0600

    first commit

---
+++ src/main/java/mx/osdasanchez/resourceserver/controller/ResourceController.java
@@ -0,0 +1,26 @@
+package mx.osdasanchez.resourceserver.controller;
+
+import mx.osdasanchez.resourceserver.dto.MessageDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceController {
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER', 'OIDC_USER')")
+    public ResponseEntity<MessageDto> user(Authentication authentication){
+        return ResponseEntity.ok(new MessageDto("Hello " + authentication.getName()));
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<MessageDto> admin(Authentication authentication){
+        return ResponseEntity.ok(new MessageDto("Hello Mr. " + authentication.getName()));
+    }
+}
---
+++ src/main/java/mx/osdasanchez/resourceserver/dto/MessageDto.java
@@ -0,0 +1,4 @@
+package mx.osdasanchez.resourceserver.dto;
+
+public record MessageDto(String message) {
+}
